c51984140d292d294883a95d62867ab26aa56f33
Daniel García <dani-garcia@users.noreply.github.com>
2019-04-03 15:44:07 +0200

    hide subscription plans

--- apps/web/src/app/billing/organizations/organization-billing-history-view.component.ts
+++ apps/web/src/app/billing/organizations/organization-billing-history-view.component.ts
@@ -52,6 +52,7 @@ export class OrgBillingHistoryViewComponent implements OnInit, OnDestroy {
 
     this.loading = true;
 
+    /* disable billing history
     const openInvoicesPromise = this.organizationBillingApiService.getBillingInvoices(
       this.organizationId,
       "open",
@@ -85,6 +86,7 @@ export class OrgBillingHistoryViewComponent implements OnInit, OnDestroy {
       openInvoices.length <= pageSize ||
       paidInvoices.length <= pageSize ||
       transactions.length <= pageSize;
+    */
 
     this.loading = false;
   }
