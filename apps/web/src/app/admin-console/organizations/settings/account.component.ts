7fc803df613eab24411d71ba5d4e12e5733ea6fe
Stefan Melmuk <stefan.melmuk@gmail.com>
2024-02-05 21:51:25 +0100

    disable flexible collections
    Co-authored-by: BlackDex <black.dex@gmail.com>

--- apps/web/src/app/admin-console/organizations/settings/account.component.ts
+++ apps/web/src/app/admin-console/organizations/settings/account.component.ts
@@ -179,6 +179,7 @@ export class AccountComponent implements OnInit, OnDestroy {
   };
 
   submitCollectionManagement = async () => {
+    return; // flexible collections are not supported by Vaultwarden
     const request = new OrganizationCollectionManagementUpdateRequest();
     request.limitCollectionCreation =
       this.collectionManagementFormGroup.value.limitCollectionCreation;
