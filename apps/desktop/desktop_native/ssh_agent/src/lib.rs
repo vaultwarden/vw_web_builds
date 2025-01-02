d37ee53099568acb2cdf7a6bf08dfdc432ade5da
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-01-02 19:05:42 +0100

    remove other clients

--- apps/desktop/desktop_native/ssh_agent/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Bitwarden SSH Agent implementation
-//!
-//! <https://www.ietf.org/archive/id/draft-miller-ssh-agent-11.html#RFC4253>
-
-#![allow(dead_code)] // TODO remove when all code is used in follow-up PR
-
-mod crypto;
