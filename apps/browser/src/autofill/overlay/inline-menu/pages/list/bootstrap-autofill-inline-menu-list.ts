df5d3a0239824726a88a333261f060e97ac18cdb
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-01-02 19:05:42 +0100

    remove other clients

--- apps/browser/src/autofill/overlay/inline-menu/pages/list/bootstrap-autofill-inline-menu-list.ts
+++
@@ -1,9 +0,0 @@
-import { AutofillOverlayElement } from "../../../../enums/autofill-overlay.enum";
-
-import { AutofillInlineMenuList } from "./autofill-inline-menu-list";
-
-require("./list.scss");
-
-(function () {
-  globalThis.customElements.define(AutofillOverlayElement.List, AutofillInlineMenuList);
-})();
