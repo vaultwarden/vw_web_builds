862cdd86fb6362007bfbaba9fe5f80a54e038d8d
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-01-02 18:41:19 +0100

    remove non-free code from bitwarden_license

--- bitwarden_license/bit-common/src/dirt/organization-integrations/models/organization-integration-service-type.ts
+++
@@ -1,9 +0,0 @@
-export const OrganizationIntegrationServiceName = Object.freeze({
-  Blumira: "Blumira",
-  CrowdStrike: "CrowdStrike",
-  Datadog: "Datadog",
-  Huntress: "Huntress",
-} as const);
-
-export type OrganizationIntegrationServiceName =
-  (typeof OrganizationIntegrationServiceName)[keyof typeof OrganizationIntegrationServiceName];
