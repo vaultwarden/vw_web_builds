e1cca3e9f7cea104f757a99b3730c6e454a08b58
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-01-02 19:05:42 +0100

    remove other clients

--- apps/desktop/src/app/tools/send/guards/unsaved-send-edits.guard.ts
+++
@@ -1,7 +0,0 @@
-import { CanDeactivateFn } from "@angular/router";
-
-import { SendComponent } from "../send.component";
-
-export const unsavedSendEditsGuard: CanDeactivateFn<SendComponent> = async (component) => {
-  return component.saveUnsavedSendEdits();
-};
