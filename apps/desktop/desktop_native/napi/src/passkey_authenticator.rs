1abf55039c0ebc4ed63581897efcd478c98af813
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-01-02 19:05:42 +0100

    remove other clients

--- apps/desktop/desktop_native/napi/src/passkey_authenticator.rs
+++
@@ -1,9 +0,0 @@
-#[napi]
-pub mod passkey_authenticator {
-    #[napi]
-    pub fn register() -> napi::Result<()> {
-        crate::passkey_authenticator_internal::register().map_err(|e| {
-            napi::Error::from_reason(format!("Passkey registration failed - Error: {e} - {e:?}"))
-        })
-    }
-}
