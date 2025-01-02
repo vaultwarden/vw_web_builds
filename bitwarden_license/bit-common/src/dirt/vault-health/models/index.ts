db98d79702fc45c3a1da21892ed83d481f00030c
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-01-02 18:41:19 +0100

    remove non-free code from bitwarden_license

--- bitwarden_license/bit-common/src/dirt/vault-health/models/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./risk-category";
-export * from "./view/vault-health-report.view";
--- bitwarden_license/bit-common/src/dirt/vault-health/services/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./abstractions/vault-health-report.service";
-export * from "./implementations/default-vault-health-report.service";
