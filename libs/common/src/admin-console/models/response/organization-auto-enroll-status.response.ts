caf8e2fd9f6b0433914bbbc8298cdac20f4aad7f
Timshel <timshel@users.noreply.github.com>
2025-04-03 18:35:19 +0200

    Dummy Identifier value to skip screen

--- libs/common/src/admin-console/models/response/organization-auto-enroll-status.response.ts
+++ libs/common/src/admin-console/models/response/organization-auto-enroll-status.response.ts
@@ -2,11 +2,13 @@ import { BaseResponse } from "../../../models/response/base.response";
 
 export class OrganizationAutoEnrollStatusResponse extends BaseResponse {
   id: string;
+  identifier: string;
   resetPasswordEnabled: boolean;
 
   constructor(response: any) {
     super(response);
     this.id = this.getResponseProperty("Id");
+    this.identifier = this.getResponseProperty("Identifier");
     this.resetPasswordEnabled = this.getResponseProperty("ResetPasswordEnabled");
   }
 }
